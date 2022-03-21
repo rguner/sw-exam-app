904a205e14110d0d370509fea9c2f03cf56cf55b
Ramazan Guner <rguner@gmail.com>
2022-03-21 23:51:58 +0300

    interceptor exception removed

--- src/main/java/com/example/demo/source/interceptor/LoggingInterceptor.java
+++ src/main/java/com/example/demo/source/interceptor/LoggingInterceptor.java
@@ -12,18 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class LoggingInterceptor implements HandlerInterceptor {
 
-   @Override
-   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-      return true;
-   }
-   @Override
-   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-   }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        return true;
+    }
 
-   @Override
-   public void afterCompletion
-      (HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+    }
 
-      log.debug("[Request: {} {}, Response: {}]", request.getMethod(), request.getRequestURI(),  response.getStatus());
-   }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) {
+
+        log.debug("[Request: {} {}, Response: {}]", request.getMethod(), request.getRequestURI(), response.getStatus());
+    }
 }
