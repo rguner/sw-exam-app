afd3290e378656f278594f06d2569b440c386a3a
Ramazan Guner <rguner@gmail.com>
2022-03-21 23:41:32 +0300

    interceptor added

---
+++ src/main/java/com/example/demo/source/configuration/InterceptorConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.demo.source.configuration;
+
+import com.example.demo.source.interceptor.LoggingInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfigurer {
+
+    final
+    LoggingInterceptor loggingInterceptor;
+
+    public InterceptorConfiguration(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+
+}
---
+++ src/main/java/com/example/demo/source/interceptor/LoggingInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.demo.source.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+public class LoggingInterceptor implements HandlerInterceptor {
+
+   @Override
+   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+      return true;
+   }
+   @Override
+   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+   }
+
+   @Override
+   public void afterCompletion
+      (HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
+
+      log.debug("Request: {} {}, Response: {}", request.getMethod(), request.getRequestURI(),  response.getStatus());
+   }
+}
