2a834121809edf51f1497944e94e5bf24ddcd696
Ramazan Guner <rguner@gmail.com>
2022-03-21 18:08:08 +0300

    generated value to sequence

--- src/main/java/com/example/demo/source/model/Customer.java
+++ src/main/java/com/example/demo/source/model/Customer.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Data
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private Integer age;
