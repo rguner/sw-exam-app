c180727075b9b112500cdc5cdfa08c6178d36c4b
Ramazan Guner <rguner@gmail.com>
2022-03-24 17:32:25 +0300

    spring boot test without server

--- src/test/java/com/example/demo/source/controller/CustomerControllerWithSpringBootTestWithoutServerTest.java
+++ src/test/java/com/example/demo/source/controller/CustomerControllerWithSpringBootTestWithoutServerTest.java
@@ -7,7 +7,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,9 +25,9 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
 
-//@AutoConfigureJsonTesters
-@WebMvcTest(CustomerController.class)
-public class CustomerControllerWithSpringContextTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerControllerWithSpringBootTestWithoutServerTest {
 
     @Autowired
     private MockMvc mvc;
